f53c5ed5e59175227f28f636fcb3ae979fac84c2
AcV134 <anirudh.c.vinayan@gmail.com>
2026-08-05 10:08:23 +0000

    Edit ssc_metric.py to consider IoU and mIoU in gaze area and non gaze area seperately

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ export PYTHONPATH=`pwd`:$PYTHONPATH
 export LD_LIBRARY_PATH=/home/anirudh/miniconda3/envs/disc/lib:$LD_LIBRARY_PATH
 
 #For training, uncomment this command
-python tools/train.py trainer.devices=1 use_gaze=True
+# python tools/train.py trainer.devices=1 use_gaze=True
 
 #For validation, uncomment this command
-# python tools/train.py resume.ckpt_path='/home/models/DISC/adl-project-gaze/pretrain/DISC_KITTI360.ckpt' validate=true trainer.devices=1 
+python tools/train.py resume.ckpt_path='/home/models/DISC/adl-project-gaze/outputs/DISC/DISC-v1-360/Gaze_ckpt/e4_miou0.1791.ckpt' validate=true trainer.devices=1
 
 #for inference, uncomment this command
 # python tools/test.py ++ckpt_path='/home/models/DISC/adl-project-gaze/pretrain/DISC_KITTI360.ckpt' trainer.devices=1 
